9adabeae5e8fe0dc38d07d10fd3d23817e02f7f9
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2024-05-11 20:22:49 -0600

    fir funtion declared, needs testing

--- QLearner.py
+++ QLearner.py
@@ -16,6 +16,7 @@ from keras.models import model_from_json
 
 
 
+
 class DEEPQ:
     prices={
             'p':1,
@@ -32,11 +33,12 @@ class DEEPQ:
             'K':-90,
             'None':0
         }
-    def __init__(self,epsilon=0.9,existing_model = None,general_moves = None):
+    def __init__(self,epsilon=0.9,gamma=0.005,existing_model = None,general_moves = None):
         '''
         For simplifiaction sake, the states is a 1d array of values for each square
         the value is given by the prices dictionary
         '''
+        self.gamma = gamma
         self.epsilon = epsilon
         self.graph = tf.getdefault_graph()
         self.state =  np.zeros((1,65))
@@ -58,7 +60,7 @@ class DEEPQ:
             self.model.load_weights("model.h5")
             self.model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
             self.model._make_predict_function()
-    def pick_move(self,board:chess.Board):
+    def pick_move(self,board:chess.Board,allow_epsilon=False):
         '''
         returns the best move based on the current board state
         ...
@@ -66,12 +68,17 @@ class DEEPQ:
         Params
         ------
         board : chess.Board
-
+        allow_epsilon: boolean
+            default False
+            allows epsilon to be used for random moves
         Returns
         ------
         move : chess.Move object to be pushed to the board
         None: there is no move to be pushed ( extremely unlikely )
         '''
+        if allow_epsilon:
+            if np.random.random <= self.epsilon:
+                return board.legal_moves[np.random.randint(0,board.legal_moves.count())]
         #factor
         Q = {} #the q table for the movements
         factor = 1 if board.turn else -1 
@@ -80,11 +87,50 @@ class DEEPQ:
             self.state = factor*DEEPQ.prices[str(board.piece_at(i))]
         # check the legal moves
         for move in board.legal_moves:
-            self.state[0][64] = self.general_moves[str(move)]
+            try :
+                self.state[0][64] = self.general_moves[str(move)]
+            except KeyError:
+                self.general_moves[str(move)] = len(self.general_moves)
+                self.state[0][64] = self.general_moves[str(move)]
             with self.graph.as_default():
                 Q[str(move)] = self.model.predict(self.state)
 
         best = max(Q.items(),key=operator.itemgetter(1))[0]
         return chess.Move().from_uci(best)
-    def fit(self):
-        pass
+    def fit(self,win_states, win_actions, l_states, l_actions,win_weights=1, l_weights=-1.2): 
+        '''
+        fit the model given the states and moves
+        it will punish any move on the looser input
+        effectively avoiding them
+        ...
+
+        Params
+        -----
+
+        win_states : np.array (1,65) 
+            represent the board as a np array of weighs given the piece of the winner
+        win_actions : python list[string]
+            represent the move as uci , example a7b6 , move from a7 to b6 , the boeard handles the piece and it's legallity 
+
+        l_states : np.array (1,65) 
+            represent the board as a np array of weighs given the piece of the looser
+        l_actions : python list[string]
+            represent the move as uci , example a7b6 , move from a7 to b6 , the boeard handles the piece and it's legallity 
+
+        '''
+        #reward all the moves that lead to victory
+        for i in range(len(win_states)):
+            win_states[i][0][64]=self.get_int(win_actions[i])
+            self.model.train_on_batch(np.array(win_states[i]),self.model.predict(np.array(win_states[i]))+win_weights*(self.gamma*i))
+        for i in range(len(l_states)):
+            l_states[i][0][64]=self.get_int(l_actions[i])
+            self.model.train_on_batch(np.array(l_states[i]),self.model.predict(np.array(l_states[i]))+l_weights*(self.gamma*i))
+
+    def _get_move_id(self, move:str):
+        #giving the id of the move as int value for the neural network
+        try:
+            return self.general_moves[move]
+        except KeyError:
+            self.general_moves[move] = len(self.general_moves)
+            return self.general_moves[move]
+    
